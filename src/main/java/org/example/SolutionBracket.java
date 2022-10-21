c89e3058f226151fdaa19ff762775989d40ba0a9
yejinstar <gyj5586@gmail.com>
2022-10-21 10:59:46 +0900

    괄호풀기 - 스택

--- src/main/java/org/example/SolutionBracket.java
+++ src/main/java/org/example/SolutionBracket.java
@@ -1,19 +1,43 @@
 package org.example;
 
+import java.util.Stack;
+
 class SolutionBracket {
     boolean solution(String s) {
         boolean answer = false;
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
 
-        while(s.indexOf("()") >= 0) {
-            s = s.replace("()","");
+        for (int i = 0; i < n; i++) {
+            if(st.isEmpty()){
+                st.push(s.charAt(i));
+            }
+            else if(st.peek()=='(' && s.charAt(i) == ')'){
+                st.pop();
+            } else {
+                st.push(s.charAt(i));
+            }
         }
-
-        if (s.length() == 0){
-            answer = true;
-        } else{
-            answer = false;
-        }
+        answer = st.isEmpty();
         return answer;
     }
 }
+
+// -------- Stack 없이 풀기 --------
+//
+//    boolean solution(String s) {
+//        boolean answer = false;
+//
+//        while(s.indexOf("()") >= 0) {
+//            s = s.replace("()","");
+//        }
+//
+//        if (s.length() == 0){
+//            answer = true;
+//        } else{
+//            answer = false;
+//        }
+//        return answer;
+//    }
+
 
