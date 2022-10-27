8762e1a526e68bed167dfa4211b9ae891ad91157
yejinstar <gyj5586@gmail.com>
2022-10-27 09:26:24 +0900

    완주하지 못한 선수

---
+++ src/main/java/org/example/java_1025/HashSolution.java
@@ -0,0 +1,20 @@
+package org.example.java_1025;
+
+import java.util.HashMap;
+
+public class HashSolution {
+
+    HashMap<String, Integer> map = new HashMap<>();
+
+    public String solution(String[] participant, String[] completion){
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        HashSolution hs = new HashSolution();
+        System.out.println(hs.solution(participant,completion));
+    }
+}
