4e125e7978f20436b511e09bfc08658782b192cd
yejinstar <gyj5586@gmail.com>
2022-10-27 10:30:24 +0900

    완주하지 못한 선수

--- src/main/java/org/example/java_1025/HashSolution.java
+++ src/main/java/org/example/java_1025/HashSolution.java
@@ -1,13 +1,33 @@
 package org.example.java_1025;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashSolution {
-
-    HashMap<String, Integer> map = new HashMap<>();
-
     public String solution(String[] participant, String[] completion){
         String answer = "";
+
+        Map<String, Integer> memo = new HashMap<>();
+
+        for (int i = 0; i < participant.length; i++) {
+            String key = participant[i];
+            if(!memo.containsKey(key)){
+                memo.put(key, 0);
+            }
+            memo.put(key, memo.get(key)+1);
+        }
+
+        for (int i = 0; i < completion.length; i++) {
+            String key = completion[i];
+            memo.put(key, memo.get(key)-1);
+        }
+
+        for (String key : memo.keySet()) {
+            if (memo.get(key) == 1) {
+                return key;
+            }
+        }
+
         return answer;
     }
 
