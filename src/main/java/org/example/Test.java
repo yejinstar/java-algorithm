14eede3403dbb12b35988bd7e0868293ba4de36c
yejinstar <gyj5586@gmail.com>
2022-10-31 09:44:56 +0900

    프로그래머스 - 모의고사 (로직 떠올리기)

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Test {
+
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+
+        int[] studentA = {1, 2, 3, 4, 5}; // A가 찍는 법
+        int[] studentB = {2, 1, 2, 3, 2, 4, 2, 5}; // B가 찍는 법
+        int[] studentC = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // C가 찍는 법
+
+        int[] score = {0, 0, 0}; // 학생들의 점수
+
+        for (int i = 0; i < 3 ; i++) { // 학생들 점수
+            for (int j = 0; j < answers.length ; j++) { // 문제 수 만큼 반복문 돌리면서 맞으면 점수 추가
+
+            }
+        }
+
+        return answer;
+    }
+
+
 }
