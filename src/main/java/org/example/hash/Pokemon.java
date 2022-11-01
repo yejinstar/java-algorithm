b0796f5ff4a00b2444353f8acdab4ac798ddfa5d
yejinstar <gyj5586@gmail.com>
2022-11-01 09:21:08 +0900

    프로그래머스 - Pokemon (완성)

--- src/main/java/org/example/hash/Pokemon.java
+++ src/main/java/org/example/hash/Pokemon.java
@@ -9,15 +9,15 @@ public class Pokemon {
         int answer = 0;
 
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) { // set에 중복없이 실험실에 있는 폰켓몬의 종류 번호를 넣어준다.
             set.add(nums[i]);
         }
-        int n = nums.length / 2;
+        int n = nums.length / 2; // 데려갈 수 있는 최대 폰켓몬의 수
 
-        if (set.size() < n) {
-            answer = set.size();
+        if (set.size() < n) { // 데려갈 수 있는 숫자보다 실험실에 있는 폰켓몬의 종류가 작으면
+            answer = set.size(); // 실험실에 있는 종류가 가장 많은 종류
         } else {
-            answer = n;
+            answer = n; // 실험실에 있는 폰켓몬의 종류가 데려갈 수 있는 수보다 크면 데려갈 수 있눈 숫자가 가장 많은 종류
         }
         return answer;
     }
