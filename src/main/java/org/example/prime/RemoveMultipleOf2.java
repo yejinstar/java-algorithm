c2da155560308df76d0c5980f00ac92a57aba464
yejinstar <gyj5586@gmail.com>
2022-11-03 09:24:23 +0900

    소수 구하기 에라토스테네스 체 알고리즘 - 배열 사용(배수 출력 해보기)완성했지만 시간 초과

--- src/main/java/org/example/prime/RemoveMultipleOf2.java
+++ src/main/java/org/example/prime/RemoveMultipleOf2.java
@@ -13,15 +13,17 @@ public class RemoveMultipleOf2 {
             arrCheck[idx] = true;
             idx++;
         }
-
+        // 루트n 만큼 돌리면서 배수들 false 처리하기
         for (int i = 2; i*i <= n; i++) {
             for (int j = 0; j < n; j++) {
                 if(arrN[j]%i == 0 && arrN[j] > i){
                     arrCheck[j] = false;
+                    System.out.printf("%d ", arrN[j]); // 소수의 배수들 출력해보기
                 }
             }
+            System.out.println();
         }
-
+        // true 가 소수니까 소수의 갯수 구하기
         for (boolean c:
              arrCheck) {
             if (c == true){
