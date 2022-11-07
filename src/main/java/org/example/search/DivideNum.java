8b4cac8fe793c51871e1afa4d24f7d1af563f19c
yejinstar <gyj5586@gmail.com>
2022-11-07 10:21:57 +0900

    나누어 떨어지는 숫자 배열 - 완성 코드 보기좋게 정렬

--- src/main/java/org/example/search/DivideNum.java
+++ src/main/java/org/example/search/DivideNum.java
@@ -1,7 +1,7 @@
 package org.example.search;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 
 public class DivideNum {
@@ -11,23 +11,29 @@ public class DivideNum {
         int N = arr.length;
         List<Integer> list = new ArrayList<Integer>();
 
+        //나누어 떨어지면 리스트에 저장
         for (int i = 0; i < N; i++) {
             if (arr[i] % divisor == 0) {
                 list.add(arr[i]);
             }
         }
-        list.sort(Comparator.naturalOrder());
         int listN = list.size();
+
+        //리스트에 나누어 지는 수가 없을 경우
         if (listN == 0){
-            answer = new int[1];
-            answer[0] = -1;
+            /*answer = new int[1];
+            answer[0] = -1;*/
+            answer = new int[]{-1};
             return answer;
         }
+
+        //리스트를 배열로
         answer = new int[listN];
         for (int i = 0; i < listN; i++) {
             answer[i] = list.get(i);
         }
 
+        Arrays.sort(answer);
         return answer;
     }
 }
