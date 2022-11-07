0244ba964146a9405de6e6af22f565800cdb778d
yejinstar <gyj5586@gmail.com>
2022-11-07 09:24:59 +0900

    하샤드 수 - 완성

--- src/main/java/org/example/search/HarshadTest.java
+++ src/main/java/org/example/search/HarshadTest.java
@@ -4,7 +4,16 @@ public class HarshadTest {
     public boolean solution(int x) {
         boolean answer = true;
 
+        int sumOfDigit = 0;
+        int oringinX = x;
+        while(x>0){ // 주어진 수 x의 자릿수르 모두 더한다
+            sumOfDigit += x % 10;
+            x /= 10;
+        }
 
+        if (oringinX % sumOfDigit != 0) { // 자릿수의 합으로 나눠지면 하샤드 수니까
+            answer = false; // 안 나눠지면 false로 만들기 
+        }
 
         return answer;
     }
