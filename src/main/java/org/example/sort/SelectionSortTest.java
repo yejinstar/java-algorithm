ffd06104a1729e4a485a278068fbf66d0c871cdf
yejinstar <gyj5586@gmail.com>
2022-11-09 10:36:51 +0900

    선택정렬(오름차순, 내림차순) 인터페이스 사용해서

--- src/main/java/org/example/sort/SelectionSortTest.java
+++ src/main/java/org/example/sort/SelectionSortTest.java
@@ -2,15 +2,19 @@ package org.example.sort;
 
 import java.util.Arrays;
 
+interface StatementStrategy{
+    boolean compare(int a, int b);
+}
+
 public class SelectionSortTest {
-    public int[] SelectionSort(int arr[]){
+    public int[] SelectionSort(int arr[], StatementStrategy stmt){
 
         int minIdx = 0;
         int tmp;
         for (int i = 0; i < arr.length-1; i++) {
             minIdx = i;
             for (int j = i+1; j < arr.length; j++) {
-                if (arr[minIdx] > arr[j]) {
+                if (stmt.compare(arr[minIdx] , arr[j])) {
                     minIdx = j;
                 }
             }
@@ -25,6 +29,7 @@ public class SelectionSortTest {
     public static void main(String[] args) {
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
         SelectionSortTest sst = new SelectionSortTest();
-        System.out.println(Arrays.toString(sst.SelectionSort(arr)));
+        System.out.println(Arrays.toString(sst.SelectionSort(arr, (a, b) -> a > b))); //오름차순
+        System.out.println(Arrays.toString(sst.SelectionSort(arr, (a, b) -> a < b))); //내림차순
     }
 }
