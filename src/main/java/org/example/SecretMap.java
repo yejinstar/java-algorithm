ad0d29cb0de5b696fc95156d59f61a9ef014b63a
yejinstar <gyj5586@gmail.com>
2022-11-11 09:18:46 +0900

    프로그래머스 [1차] 비밀지도 - 주석으로 풀이방법 떠올리기

---
+++ src/main/java/org/example/SecretMap.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class SecretMap {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        /*
+        * 1. 5*5 배열을 만든다
+        * 2. 들어온 값을 5번 반복하면서 2진수로 만든다
+        * 3. 한 칸에 0이랑 1을 넣어준다
+        * 4. 출력할 때 1인 곳은 #으로 표시해서 5번 출력한다.
+        * */
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
