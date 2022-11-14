72b86c77814fadf113e50e1d6a702979b6a0c1cc
yejinstar <gyj5586@gmail.com>
2022-11-14 10:49:57 +0900

    에라토스테네스의 체 소수찾기 복습 - 완성

--- src/main/java/org/example/prime/RewindPrime.java
+++ src/main/java/org/example/prime/RewindPrime.java
@@ -8,22 +8,31 @@ public class RewindPrime {
         int answer = 0;
         //
 
-        int[] arrN = new int[n];
+        int[] arrN = new int[n-1];
         int idx = 0;
 
+        // 2~50까지 채우기
         for (int i = 2; i <= n ; i++) {
             arrN[i-2] = i;
         }
-
-        for (int i = 2; i <= n ; i++) {
-            arrN[i-2] = i;
-            if (arrN[i-2] % 2 == 0 && arrN[i-2] > 2){
-                arrN[i-2] = 0;
+        // 2보다 큰 2의 배수들 지우기
+        for (int i = 2; i*i <=n ; i++) {
+            for (int j = i-2; j <= n-2 ; j += i) {
+                if (arrN[j] > i){
+                    arrN[j] = 0;
+                }
             }
         }
 
         //
         System.out.println(Arrays.toString(arrN));
+
+        for (int i = 0; i < arrN.length; i++) {
+            if (arrN[i]!=0){
+                answer += 1;
+            }
+        }
+
         return answer;
     }
 
