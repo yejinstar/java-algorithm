6cc61e93b3fd2bee3bbfa6f7233a80ef79bcf1e0
yejinstar <gyj5586@gmail.com>
2022-11-24 10:23:22 +0900

    코드업 1859 재귀로 피라미드 별찍기 성공

--- src/main/java/org/example/recursion/CodeUp1859.java
+++ src/main/java/org/example/recursion/CodeUp1859.java
@@ -3,11 +3,11 @@ package org.example.recursion;
 import java.util.Scanner;
 
 public class CodeUp1859 {
-    public static void fibo(int input) {
+    public static void recur(int input) {
         if (input == 0) {
             return;
         }
-        fibo(input-1);
+        recur(input-1);
         star(input);
         System.out.printf("\n");
     }
@@ -23,6 +23,6 @@ public class CodeUp1859 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        fibo(input);
+        recur(input);
     }
 }
