648352a96fe23dd8704566f5038c5242355b7aca
yejinstar <gyj5586@gmail.com>
2022-11-28 09:24:49 +0900

    1128 실습2 - 모든 알파벳 조합 출력하기

--- src/main/java/org/example/AlphabetPrintTest.java
+++ src/main/java/org/example/AlphabetPrintTest.java
@@ -8,7 +8,17 @@ public class AlphabetPrintTest {
         }
     }
 
+    public static void alphabetPrint2(){
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                System.out.printf("%c %c\n", 'A' + i, 'A' + j);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         alphabetPrint();
+        System.out.println("-------------");
+        alphabetPrint2();
     }
 }
