b59f6605559cad4f9a8269b069fb7e5494b19c3f
yejinstar <gyj5586@gmail.com>
2022-11-28 10:07:17 +0900

    1128 실습3 - 재귀로 모든 알파벳 조합 출력하기

---
+++ src/main/java/org/example/AlphabetPrintTestRecursion.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class AlphabetPrintTestRecursion {
+
+    public static void alphabetPrint(char C) {
+
+        if (C > 'Z')
+            return;
+        System.out.printf("%c", C);
+        System.out.println();
+        alphabetPrint((char) (C + 1));
+    }
+
+    public static void alphabetPrintSecond(char C, char two) {
+        if (two > 'Z')
+            return;
+        System.out.printf("%c %c\n", C, two);
+        alphabetPrintSecond(C, (char) (two + 1));
+    }
+
+    public static void alphabetPrintFirst(char C) {
+
+        if (C > 'Z')
+            return;
+
+        alphabetPrintSecond(C, 'A');
+        alphabetPrintFirst((char) (C + 1));
+    }
+
+    public static void main(String[] args) {
+        alphabetPrint('A');
+        System.out.println("-------------");
+        alphabetPrintFirst('A');
+    }
+}
