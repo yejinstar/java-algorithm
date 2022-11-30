a00f654e1d9cd4a335a5591d2c086608c3aedef3
yejinstar <gyj5586@gmail.com>
2022-11-30 10:19:21 +0900

    프로그래머스 약수의 합 - 완료

---
+++ src/main/java/org/example/FindDivisorSum.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class FindDivisorSum {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
