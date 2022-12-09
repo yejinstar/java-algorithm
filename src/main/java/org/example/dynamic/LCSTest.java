ef977d8fa03edd52b7fe5ef84ae102d5486c4542
yejinstar <gyj5586@gmail.com>
2022-12-09 10:01:00 +0900

    feat: LCS 길이 구하기 (dynamic programming)

--- src/main/java/org/example/dynamic/LCSTest.java
+++ src/main/java/org/example/dynamic/LCSTest.java
@@ -3,12 +3,17 @@ package org.example.dynamic;
 public class LCSTest {
 
     public static void LCS(String str1, String str2) {
-        int[][] dp = new int[str2.length()][str1.length()];
+        int[][] dp = new int[str2.length()+1][str1.length()+1];
 
-        for (int i = 0; i < str2.length(); i++) {
-            for (int j = 0; j < str1.length(); j++) {
-                System.out.printf("i: %s, j: %s\n",str2.charAt(i),
-                        str1.charAt(j));
+        for (int i = 1; i <= str2.length(); i++) {
+            for (int j = 1; j <= str1.length(); j++) {
+                if (str2.charAt(i-1) == str1.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                System.out.printf("i: %s, j: %s, dp: %d\n", str2.charAt(i-1),
+                        str1.charAt(j-1), dp[i][j]);
             }
         }
     }
